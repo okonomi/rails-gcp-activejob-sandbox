ebc76cd84583a3871931a8acfbb6b4d0a9d07440
okonomi <okonomi@oknm.jp>
2020-09-22 17:45:13 +0900

    Pub/Sub push trigger

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,7 +1,28 @@
+require 'base64'
+require 'json'
+
 class HelloController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     HelloJob.perform_later params[:name]
 
     render plain: "Enqueued!"
+  end
+
+  def worker
+    data = Base64.decode64(message_params[:data])
+    message = JSON.parse(data)
+    message['job_class'].constantize.public_send(:perform_now, *message['arguments'])
+
+    render plain: "Executed!"
+  rescue e
+    p e
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:data)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/hello/:name' => 'hello#index'
+  post '/worker' => 'hello#worker'
 end
